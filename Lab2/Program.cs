a68159b97c22ac0a9d8dcb561200fd5b9897445d
Alex <a.bridnia@gmail.com>
2026-04-25 19:29:01 +0300

    Main code

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -3,6 +3,14 @@
 internal class Program
 {
     private static void Main(string[] args)
-    {}
+    {
+        var text = new StringText("!!!Hello, Team! Hi!!! Fruits: banana,apple,orange. Animals:red parrot;green Monkey\n Years — twenty-one... ?What next?");
+        var sortedSentences = text.GetSortedSentences();
+
+        Console.WriteLine($"1. Text:\n{text.Data}\n");
+        Console.WriteLine($"2. Sorted sentences:\n{string.Join("\n", sortedSentences)}");
+
+        Console.ReadKey();
+    }
 }
 
